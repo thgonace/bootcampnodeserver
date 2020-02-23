023e2c6f5a6a9d7ea12df1f271864fd3d83c203e
thgonace <joshua.byer.work@gmail.com>
2020-02-23 17:10:23 -0500

    Create angularanimate.js

---
+++ angularanimate.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+app.use(cors())
+app.route('/pages/:pagenumber').get(function (req, res) {
+    
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://user:aEWrj65HzdzbWGd@cluster0-pgykr.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        if (err) throw err;
+        const collection = client.db("pages").collection("pages").find({example_number:req.param("pagenumber")}).toArray(function (err,cursor) {
+            if (err) throw err;
+            res.send(cursor);            
+            client.close();
+        });        
+    });
+});
+var server = app.listen(3000, function () { }); 
